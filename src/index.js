54f45e2fa3fe619b1a95feee2e19cdaea9e50e03
Christian Zillmann <christian.zillmann>
2022-12-12 21:46:38 +0100

    increase dnd version

--- src/index.js
+++ src/index.js
@@ -1,33 +1,35 @@
 import React, { Component } from 'react';
-import { DragDropContext } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
 import Tab from './Tab';
 import './style.css'
 import './palette.css'
+import { DndProvider } from "react-dnd";
 
 class Tabs extends Component {
-  render() {
-    const { tabs } = this.props;
-    return (
-      <div className="react-tabs-container">
-        {tabs.map((tab, i) => (
-          <Tab
-            key={tab.id}
-            index={i}
-            id={tab.id}
-            content={tab.content}
-            moveTab={this.props.moveTab}
-            selectTab={this.props.selectTab}
-            closeTab = {this.props.closeTab}
-            active={tab.active}
-          />
-        ))}
-        <div className="react-tabs-child">
-            {React.Children.toArray(this.props.children)}
-        </div>
-      </div>
-    );
-  }
+    render() {
+        const { tabs } = this.props;
+        return (
+            <DndProvider backend={HTML5Backend}>
+                <div className="react-tabs-container">
+                    {tabs.map((tab, i) => (
+                        <Tab
+                            key={tab.id}
+                            index={i}
+                            id={tab.id}
+                            content={tab.content}
+                            moveTab={this.props.moveTab}
+                            selectTab={this.props.selectTab}
+                            closeTab={this.props.closeTab}
+                            active={tab.active}
+                        />
+                    ))}
+                    <div className="react-tabs-child">
+                        {React.Children.toArray(this.props.children)}
+                    </div>
+                </div>
+            </DndProvider>
+        );
+    }
 }
 
-export default DragDropContext(HTML5Backend)(Tabs)
+export default Tabs
