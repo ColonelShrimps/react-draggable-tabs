5aa94438a3c3e57deea3bb802406168b5fd24267
Blake Dillon <dillonb0054@uhcl.edu>
2023-08-28 03:01:16 -0500

    Changes DnD backend handling to support mobile touch detection

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
-import HTML5Backend from 'react-dnd-html5-backend';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import TouchBackend from "react-dnd-touch-backend";
 import Tab from './Tab';
 import './style.css'
 import './palette.css'
 import { DndProvider } from "react-dnd";
 
+
 class Tabs extends Component {
     render() {
         const { tabs } = this.props;
+        function useDeviceDetect() {
+            const [isTouchDevice, setIsTouchDevice] = React.useState(false);
+            React.useEffect(() => {
+                if (!window.matchMedia) return;
+                setIsTouchDevice(window.matchMedia("(pointer:coarse)").matches);
+            }, []);  
+            return isTouchDevice;
+        }
+
+  const backendForDND = useDeviceDetect() ? TouchBackend : HTML5Backend;
+
         return (
-            <DndProvider backend={HTML5Backend}>
+            <DndProvider backend={backendForDND}>
                 <div className="react-tabs-container">
                     {tabs.map((tab, i) => (
                         <Tab
